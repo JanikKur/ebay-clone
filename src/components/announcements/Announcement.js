ff8fed0ca8862e873be3099fefb8f88ecb0b7c41
JaniKur <janik.kurtz2@googlemail.com>
2022-03-04 15:06:16 +0100

    Fixed Some Errors

--- src/components/announcements/Announcement.js
+++ src/components/announcements/Announcement.js
@@ -2,13 +2,13 @@ import React from 'react'
 import TestImg from '../../assets/images/test.bmp';
 export default function Announcement({url}) {
   return (
-    <a href="" className="announcement">
+    <a href="/" className="announcement">
         <div className="announcement-image-wrapper">
-            <img className="announcement-preview-img" src={TestImg}/>
-            <p className="announcement-price">100€ VB</p>
+            <img className="announcement-preview-img" alt={"TEst"} src={TestImg}/>
+            <p className="announcement-price">59.631 € VB</p>
         </div>
         <div className="announcement-description">
-            <h3 className="announcement-title">Siemens Clinitest Rapid COViD 19 Self Test</h3>
+            <h3 className="announcement-title">Audi A5 S-line,S-tronic,Quattro,Diesel ( Details lesen )</h3>
             <label className="announcement-place">Merzig</label>
         </div>
     </a>
